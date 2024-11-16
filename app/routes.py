db56ecc4766a9f54b2523e8bbef0ffc768dd0cc9
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-17 00:12:32 +0200

    Login view function.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from app.forms import LoginForm
 from flask import render_template
 
 
@@ -17,3 +18,9 @@ def index():
         },
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
