e511d0a93d877f04aec7f0acee80d788fd324154
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-16 22:14:21 +0200

    Return complete HTML page from view function.

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,18 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, Sakhile!"
+    user = {"username": "Sakhile"}
+    return (
+        """
+        <html>
+            <head>
+                <title>Home Page - Flask Chronicles</title>
+            </head>
+            <body>
+                <h1>Hello, """
+                + user["username"]
+                + """!</h1>
+            </body>
+        </html>
+    """
+    )
