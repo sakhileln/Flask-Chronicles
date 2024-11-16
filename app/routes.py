89c1b7754577894ff8c322f72ca9409559f87a4f
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-16 22:46:40 +0200

    Parse posts to the render_template() function.

--- app/routes.py
+++ app/routes.py
@@ -18,4 +18,4 @@ def index():
             },
         },
     ]
-    return render_template("index.html", title="Home", user=user)
+    return render_template("index.html", title="Home", user=user, posts=posts)
