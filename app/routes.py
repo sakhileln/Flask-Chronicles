8e5d0c9aa128f4493c97986edea060141cc9bd23
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-16 22:45:04 +0200

    Fake posts in view function.

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,16 @@ from flask import render_template
 @app.route("/index")
 def index():
     user = {"username": "Sakhile"}
+    posts = [
+        {
+            "author": {"username": "Sakhile"},
+            "body": {"Plyaing with Flask, Jinja and some HTML!"},
+        },
+        {
+            "author": {"username": "Elon"},
+            "body": {
+                "Final preparations for Starship Flight 6 🚀 on 18 November!"
+            },
+        },
+    ]
     return render_template("index.html", title="Home", user=user)
