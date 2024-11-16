6eed967ce34eef94ecc76253792434de6d8ddb2c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-16 22:48:01 +0200

    Use strings instead of dictionary for posts body.

--- app/routes.py
+++ app/routes.py
@@ -9,13 +9,11 @@ def index():
     posts = [
         {
             "author": {"username": "Sakhile"},
-            "body": {"Plyaing with Flask, Jinja and some HTML!"},
+            "body": "Plyaing with Flask, Jinja and some HTML!",
         },
         {
             "author": {"username": "Elon"},
-            "body": {
-                "Final preparations for Starship Flight 6 🚀 on 18 November!"
-            },
+            "body": "Final preparations for Starship Flight 6 🚀 on 18 November!",
         },
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
