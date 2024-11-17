522d3cd86fcb5eb94c00a142df109a687c6e0c26
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-17 19:41:24 +0200

    Login view function logic.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 
 
 @app.route("/")
@@ -22,12 +25,16 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(
-            "Login requested for user {}, remember_me={}".format(
-                form.username.data, form.remember_me.data
-            )
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
         )
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
