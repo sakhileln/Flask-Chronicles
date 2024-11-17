db6db820c4054cab4d58036063efa97ad015e95d
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-17 12:15:47 +0200

    Posts database table and re;ationship.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime, timedelta, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from typing import Optional
@@ -14,6 +15,7 @@ class User(db.Model):
         email (str): User's email address (unique and indexed).
         password_hash (Optional[str]): Hanshed password for user.
             May be None if not set
+        posts (str): A relationship back to the Post model, representing the post
     Nethids:
         __repr__: Returns a string representation of User instance.
     """
@@ -24,6 +26,9 @@ class User(db.Model):
         sa.String(120), index=True, unique=True
     )
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(
+        back_populates="author"
+    )
 
     def __repr__(self) -> str:
         """
@@ -33,3 +38,38 @@ class User(db.Model):
             str: A string in the format "<User username>" for debugging.
         """
         return "<User {}>".format(self.username)
+
+
+class Post(db.Model):
+    """
+    A class representing a post in the database.
+
+    Attributes:
+        id (int): The primary key identifier for the post.
+        body (str): Content of the post (limited ti 140 chracters).
+        timestamp (datetime): The time when post was created, in UTC+2.
+        user_id (int): Foreign key referencing the User who created the post.
+        author (User): A relationship back to the User model, representing the author
+
+    Methods:
+        __repr__: Returns a string representing of the Post instance.
+    """
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone(timedelta(hours=2)))
+    )
+    user_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True
+    )
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the Post object.
+
+        Returns:
+            str: Astring in the format "<Post body>" for debugging.
+        """
+        return f"<Post {self.body}>"
