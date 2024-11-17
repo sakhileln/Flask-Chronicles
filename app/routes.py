a699819c69378ea1eea8fa9c55474c63a6915b85
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-17 20:06:06 +0200

    Redirect to 'next' page

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app, db
 from app.models import User
 from app.forms import LoginForm
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
+from urllib.parse import urlsplit
 
 
 @app.route("/")
@@ -37,7 +38,10 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("index"))
+        next_page = request.args.get("next")
+        if not next_page or urlsplit(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
 
