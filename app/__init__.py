2144171c1ea54952dffc831ab0f0b1a84690ade2
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-24 14:28:34 +0200

    Fix spell check, missing quotation marks, and item separator on argument list.

--- app/__init__.py
+++ app/__init__.py
@@ -19,16 +19,16 @@ login.login_view = "login"
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
-        if app.config[MAIL_USERNAME] or app.config["MAIL_PASSWORD"]:
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'].
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'],
-            subject='⛔ Flask Chronicles Failure❗'
+            subject='⛔ Flask Chronicles Failure❗',
             credentials=auth,
             secure = secure
         )
