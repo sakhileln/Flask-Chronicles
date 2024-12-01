b6ca16924b64f53b2cc97cc88e9ac250a195e2c5
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-02 00:24:18 +0200

    Override DATBASE_URL so development database not used during tests.

--- tests.py
+++ tests.py
@@ -10,13 +10,12 @@ This suite tests the following functionalities:
 """
 
 import os
+
+os.environ["DATABASE_URL"] = "sqlite://"
 from datetime import datetime, timezone, timedelta
 import unittest
 from app import app, db
 from app.models import User, Post
-
-
-os.environ["DATABASE_URL"] = "sqlite://"
 
 
 class UserModelCase(unittest.TestCase):
