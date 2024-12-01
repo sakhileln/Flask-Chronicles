43072563dade1aaf414721e29c1140b3506ecb3c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-01 20:32:41 +0200

    Fix error on many-to-many model definition.

--- app/models.py
+++ app/models.py
@@ -46,15 +46,17 @@ class User(UserMixin, db.Model):
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc)
     )
-    following: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.followed_id = id),
-        secondaryjoin=(followers.c.followed_id = id),
-        back_populates='followers'
+    following: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates="followers",
     )
-    followers: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, primaryjoin=(followers.c.followed_id = id),
-        secondaryjoin=(followers.c.follower_id = id),
-        back_populates='following'
+    followers: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates="following",
     )
 
     def __repr__(self) -> str:
