0c1e13509687867189feac1527e9fb1af66cf105
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-08 00:16:42 +0200

    Next and previous page links.

--- app/routes.py
+++ app/routes.py
@@ -35,7 +35,6 @@ def index():
             "body": "Final preparations for Starship Flight 6 🚀 on 18 November!",
         },
     ]
-    posts = db.session.scalars(current_user.following_posts()).all()
     page = request.args.get("page", 1, type=int)
     posts = db.paginate(
         current_user.following_posts(),
@@ -43,9 +42,19 @@ def index():
         per_page=app.config["POSTS_PER_PAGE"],
         error_out=False,
     )
-
+    next_url = (
+        url_for("index", page=posts.next_num) if posts.has_next else None
+    )
+    prev_url = (
+        url_for("index", page=posts.prev_num) if posts.has_prev else None
+    )
     return render_template(
-        "index.html", title="Home Page", form=form, posts=posts.items
+        "index.html",
+        title="Home",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
     )
 
 
@@ -171,6 +180,21 @@ def explore():
     page = request.args.get("page", 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
     posts = db.paginate(
-        query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+        query,
+        page=page,
+        per_page=app.config["POSTS_PER_PAGE"],
+        error_out=False,
     )
-    return render_template("index.html", title="Explore", posts=posts.items)
+    next_url = (
+        url_for("explore", page=posts.next_num) if posts.has_next else None
+    )
+    prev_url = (
+        url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    )
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
