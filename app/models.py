8b6724a0f4dbd36b331134914f8e28e605f4235d
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-09 20:54:18 +0200

    Format according to PEP8.

--- app/models.py
+++ app/models.py
@@ -25,12 +25,8 @@ from app import db, login
 followers = sa.Table(
     "followers",
     db.metadata,
-    sa.Column(
-        "follower_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True
-    ),
-    sa.Column(
-        "followed_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True
-    ),
+    sa.Column("follower_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+    sa.Column("followed_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
 )
 
 
@@ -51,13 +47,9 @@ class User(UserMixin, db.Model):
 
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True)
-    email: so.Mapped[str] = so.mapped_column(
-        sa.String(120), index=True, unique=True
-    )
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    posts: so.WriteOnlyMapped["Post"] = so.relationship(
-        back_populates="author"
-    )
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc)
@@ -116,9 +108,7 @@ class User(UserMixin, db.Model):
             str: The Gravatar URL.
         """
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
-        return (
-            f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'"
-        )
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'"
 
     def follow(self, user):
         """
@@ -206,6 +196,7 @@ class User(UserMixin, db.Model):
             .order_by(Post.timestamp.desc())
         )
 
+
 # pylint: disable=too-few-public-methods
 class Post(db.Model):
     """
@@ -227,9 +218,7 @@ class Post(db.Model):
     timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc)
     )
-    user_id: so.Mapped[int] = so.mapped_column(
-        sa.ForeignKey(User.id), index=True
-    )
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     author: so.Mapped[User] = so.relationship(back_populates="posts")
 
     def __repr__(self) -> str:
@@ -240,6 +229,7 @@ class Post(db.Model):
             str: Astring in the format "<Post body>" for debugging.
         """
         return f"<Post {self.body}>"
+
 
 # pylint: disable=W0622
 # Flask-Login user loader function
