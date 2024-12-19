e3633a4bb9281c3591ba53b29b37dffed3a3b346
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-12-19 15:44:24 +0200

    Use flask babel _ for text to be translated.

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from config import Config
 
 def get_locale():
@@ -42,7 +42,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view = "login"
+login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
