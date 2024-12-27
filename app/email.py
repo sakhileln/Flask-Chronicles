418acc6c86dbafc218fcdb9fc07b57cff309b0a8
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-27 13:45:37 +0200

    Format according to PEP8.

--- app/email.py
+++ app/email.py
@@ -20,6 +20,7 @@ from flask import current_app
 from flask_mail import Message
 from app import mail
 
+
 # pylint: disable=redefined-outer-name
 def send_async_email(app, msg):
     """
@@ -60,6 +61,6 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.html = html_body
 
     # Send email asynchronously
-    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
-
-    
+    Thread(
+        target=send_async_email, args=(current_app._get_current_object(), msg)
+    ).start()
