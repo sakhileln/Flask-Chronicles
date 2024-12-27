f27d477fa4ec2c10f6d3ea287ae2443f7817e89a
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-27 14:55:11 +0200

    Modift docstring.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,7 @@
 """
-auth module for user authentication.
+main module for user handling.
 
-This module defines a Blueprint named 'auth' that manages routes and 
+This module defines a Blueprint named 'main' that manages routes and 
 views related to user authentication, including login, registration, 
 and account management.
 """
