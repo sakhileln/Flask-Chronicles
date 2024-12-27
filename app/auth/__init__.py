599d38b14c139371afd408e5a723c112fe718586
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-27 13:31:12 +0200

    Add module docstring.

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,11 @@
+"""
+auth module for user authentication.
+
+This module defines a Blueprint named 'auth' that manages routes and 
+views related to user authentication, including login, registration, 
+and account management.
+"""
+
 from flask import Blueprint
 
 bp = Blueprint('auth', __name__)
