f3b761d449040996d91cf3c96e92c924ef2a40e6
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-27 13:04:25 +0200

    Add module docstring.

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -1,5 +1,12 @@
+"""
+errors module for handling application errors.
+
+This module defines a Blueprint named 'errors' that is used to manage 
+error handling routes and their associated views in the Flask application.
+"""
+
 from flask import Blueprint
 
-bp = Blueprint('errors', __name__)
+bp = Blueprint("errors", __name__)
 
 from app.errors import handlers
