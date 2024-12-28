872e4912e2289bb7145dc4a4da246deca5c26a98
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-28 10:59:10 +0200

    Ignore [C0413]: Import 'from app.errors import handlers' should be placed at the top of the module.

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -8,5 +8,5 @@ error handling routes and their associated views in the Flask application.
 from flask import Blueprint
 
 bp = Blueprint("errors", __name__)
-
+# pylint: disable:wrong-import-position
 from app.errors import handlers
