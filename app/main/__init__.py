2002ff49644bc627e87ba1cdc98eb2f97d2d4788
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-28 11:05:07 +0200

    Fix 'pylint: disable:' to 'pylint: disable='.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -9,5 +9,5 @@ and account management.
 from flask import Blueprint
 
 bp = Blueprint("main", __name__)
-# pylint: disable:wrong-import-position
+# pylint: disable=wrong-import-position
 from app.main import routes
