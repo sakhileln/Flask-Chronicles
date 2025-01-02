bbc51cea9ca0a841588a00be8c7cbc803cab8f92
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-02 12:29:54 +0200

    Reformat module according to PEP8.

--- app/search.py
+++ app/search.py
@@ -5,6 +5,7 @@ It includes functions for adding, removing, and querying indexed documents.
 
 from flask import current_app
 
+
 def add_to_index(index, model):
     """
     Adds a model instance to the specified Elasticsearch index.
@@ -19,6 +20,7 @@ def add_to_index(index, model):
         payload[field] = getattr(model, field)
     current_app.elasticsearch.index(index=index, id=model.id, document=payload)
 
+
 def remove_from_index(index, model):
     """
     Removes a model instance from the specified Elasticsearch index.
@@ -29,6 +31,7 @@ def remove_from_index(index, model):
     if not current_app.elasticsearch:
         return
     current_app.elasticsearch.delete(index=index, id=model.id)
+
 
 def query_index(index, query, page, per_page):
     """
@@ -44,8 +47,9 @@ def query_index(index, query, page, per_page):
         return [], 0
     search = current_app.elasticsearch.search(
         index=index,
-        query={'multi_match': {'query': query, 'fields': ['*']}},
+        query={"multi_match": {"query": query, "fields": ["*"]}},
         from_=(page - 1) * per_page,
-        size=per_page)
-    ids = [int(hit['_id']) for hit in search['hits']['hits']]
-    return ids, search['hits']['total']['value']
+        size=per_page,
+    )
+    ids = [int(hit["_id"]) for hit in search["hits"]["hits"]]
+    return ids, search["hits"]["total"]["value"]
