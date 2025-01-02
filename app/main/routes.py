d809317d409ff719df372f0b525b0493e60d5332
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2025-01-02 12:29:31 +0200

    Reformat module according to PEP8.

--- app/main/routes.py
+++ app/main/routes.py
@@ -58,7 +58,7 @@ def before_request():
     'last_seen' timestamp if the user is authenticated.
     """
     if current_user.is_authenticated:
-        current_user.last_seen = datetime.now(timezone.utc) 
+        current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
         g.search_form = SearchForm()
     g.locale = str(get_locale())
@@ -265,17 +265,30 @@ def translate_text():
         "text": translate(data["text"], data["source_language"], data["dest_language"])
     }
 
-@bp.route('/search')
+
+@bp.route("/search")
 @login_required
 def search():
     if not g.search_form.validate():
-        return redirect(url_for('main.explore'))
-    page = request.args.get('page', 1, type=int)
-    posts, total = Post.search(g.search_form.q.data, page,
-                               current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
-        if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
-        if page > 1 else None
-    return render_template('search.html', title=_('Search'), posts=posts,
-                           next_url=next_url, prev_url=prev_url)
+        return redirect(url_for("main.explore"))
+    page = request.args.get("page", 1, type=int)
+    posts, total = Post.search(
+        g.search_form.q.data, page, current_app.config["POSTS_PER_PAGE"]
+    )
+    next_url = (
+        url_for("main.search", q=g.search_form.q.data, page=page + 1)
+        if total > page * current_app.config["POSTS_PER_PAGE"]
+        else None
+    )
+    prev_url = (
+        url_for("main.search", q=g.search_form.q.data, page=page - 1)
+        if page > 1
+        else None
+    )
+    return render_template(
+        "search.html",
+        title=_("Search"),
+        posts=posts,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
